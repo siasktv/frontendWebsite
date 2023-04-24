185034f8416b4b2b27282d5e8e3d90bce2deb579
siasktv <biankmbm92@gmail.com>
2023-04-24 15:39:24 -0400

    Deploy

--- src/Components/AboutUs/AboutUs.tsx
+++ src/Components/AboutUs/AboutUs.tsx
@@ -23,34 +23,34 @@ const AboutUs = () => {
     },
   }))
 
-  const LargeText = styled(Typography)(({ theme }) => ({
-    fontSize: '64px',
-    color: '#000',
-    fontWeight: '700',
-    [theme.breakpoints.down('md')]: {
-      fontSize: '32px',
-    },
-  }))
+  // const LargeText = styled(Typography)(({ theme }) => ({
+  //   fontSize: '64px',
+  //   color: '#000',
+  //   fontWeight: '700',
+  //   [theme.breakpoints.down('md')]: {
+  //     fontSize: '32px',
+  //   },
+  // }))
 
-  const SmallText = styled(Typography)(({ theme }) => ({
-    fontSize: '18px',
-    color: '#7B8087',
-    fontWeight: '500',
-    [theme.breakpoints.down('md')]: {
-      fontSize: '14px',
-    },
-  }))
+  // const SmallText = styled(Typography)(({ theme }) => ({
+  //   fontSize: '18px',
+  //   color: '#7B8087',
+  //   fontWeight: '500',
+  //   [theme.breakpoints.down('md')]: {
+  //     fontSize: '14px',
+  //   },
+  // }))
 
-  const TextFlexbox = styled(Box)(({ theme }) => ({
-    marginTop: theme.spacing(7),
-    display: 'flex',
-    justifyContent: 'space-between',
-    padding: theme.spacing(0, 5, 0, 5),
-    [theme.breakpoints.down('sm')]: {
-      flexDirection: 'column',
-      gap: theme.spacing(5),
-    },
-  }))
+  // const TextFlexbox = styled(Box)(({ theme }) => ({
+  //   marginTop: theme.spacing(7),
+  //   display: 'flex',
+  //   justifyContent: 'space-between',
+  //   padding: theme.spacing(0, 5, 0, 5),
+  //   [theme.breakpoints.down('sm')]: {
+  //     flexDirection: 'column',
+  //     gap: theme.spacing(5),
+  //   },
+  // }))
 
   const Divider = styled('div')(({ theme }) => ({
     width: '13%',
